5eb45684f95c916a73a513e3fd09cdefd9b28c77
arun-lmnas <arunkumar.ganesan@lmnas.com>
2026-06-01 05:52:55 +0000

    bootstrapped initial docs

---
+++ lenscloud/lenscloud/doctype/region/region.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, LMNAs Cloud Solutions and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class Region(NestedSet):
+	pass
